3178dd525b3ddbc7134d15e66ee7092499fd140d
Aklima2004 <iskakbaeva.aklima@mail.ru>
2024-11-05 03:18:14 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-$(function () {
-  // Обработка события потери фокуса для навигации
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
     if (screenWidth < 768) {
@@ -9,201 +8,100 @@ $(function () {
 });
 
 (function (global) {
-  var dc = {};
-  var homeHtmlUrl = "snippets/home-snippet.html";
-  var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
-  var categoriesTitleHtml = "snippets/categories-title-snippet.html";
-  var categoryHtml = "snippets/category-snippet.html";
-  var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
-  var menuItemsTitleHtml = "snippets/menu-items-title.html";
-  var menuItemHtml = "snippets/menu-item.html";
 
-  // Функция для вставки HTML
-  var insertHtml = function (selector, html) {
-    var targetElem = document.querySelector(selector);
-    targetElem.innerHTML = html;
-  };
+var dc = {};
 
-  // Показать индикатор загрузки
-  var showLoading = function (selector) {
-    var html = "<div class='text-center'>";
-    html += "<img src='images/ajax-loader.gif'></div>";
-    insertHtml(selector, html);
-  };
+var homeHtmlUrl = "snippets/home-snippet.html";
+var allCategoriesUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+var categoriesTitleHtml = "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
+var menuItemsUrl = "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
 
-  // Замена {{propName}} на propValue в строке
-  var insertProperty = function (string, propName, propValue) {
-    var propToReplace = "{{" + propName + "}}";
-    string = string.replace(new RegExp(propToReplace, "g"), propValue);
-    return string;
-  };
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
 
-  // Переключение активности между кнопками
-  var switchMenuToActive = function () {
-    // Удаление 'active' из кнопки "Home"
-    var classes = document.querySelector("#navHomeButton").className;
-    classes = classes.replace(new RegExp("active", "g"), "");
-    document.querySelector("#navHomeButton").className = classes;
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
 
-    // Добавление 'active' к кнопке "Menu"
-    classes = document.querySelector("#navMenuButton").className;
-    if (classes.indexOf("active") === -1) {
-      classes += " active";
-      document.querySelector("#navMenuButton").className = classes;
-    }
-  };
+// Return substitute of '{{propName}}'
+// with propValue in given 'string'
+var insertProperty = function (string, propName, propValue) {
+  var propToReplace = "{{" + propName + "}}";
+  string = string.replace(new RegExp(propToReplace, "g"), propValue);
+  return string;
+};
 
-  // Загрузка при первом входе
-  document.addEventListener("DOMContentLoaded", function (event) {
-    showLoading("#main-content");
-    $ajaxUtils.sendGetRequest(
-      allCategoriesUrl,
-      buildAndShowHomeHTML,  // Замена [...] на buildAndShowHomeHTML
-      true
-    );
-  });
+// Remove the class 'active' from home and switch to Menu button
+var switchMenuToActive = function () {
+  var classes = document.querySelector("#navHomeButton").className;
+  classes = classes.replace(new RegExp("active", "g"), "");
+  document.querySelector("#navHomeButton").className = classes;
 
-  // Построение HTML для главной страницы
-  function buildAndShowHomeHTML(categories) {
-    $ajaxUtils.sendGetRequest(
-      homeHtmlUrl,
-      function (homeHtml) {
-        var chosenCategory = chooseRandomCategory(categories);
-        var chosenCategoryShortName = "'" + chosenCategory.short_name + "'";  // Обернуто в кавычки
-
-        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
-        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-      },
-      false
-    );
+  classes = document.querySelector("#navMenuButton").className;
+  if (classes.indexOf("active") === -1) {
+    classes += " active";
+    document.querySelector("#navMenuButton").className = classes;
   }
+};
 
-  // Выбор случайной категории
-  function chooseRandomCategory(categories) {
-    var randomArrayIndex = Math.floor(Math.random() * categories.length);
-    return categories[randomArrayIndex];
-  }
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    allCategoriesUrl,
+    buildAndShowHomeHTML,  // Подставляем функцию, чтобы она вызывалась при получении данных
+    true
+  );
+});
 
-  // Загрузка категорий меню
-  dc.loadMenuCategories = function () {
-    showLoading("#main-content");
-    $ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
-  };
+// Builds HTML for the home page based on categories array returned from the server.
+function buildAndShowHomeHTML(categories) {
+  // Load home snippet page
+  $ajaxUtils.sendGetRequest(
+    homeHtmlUrl,
+    function (homeHtml) {
+      // STEP 2: Выбираем случайную категорию
+      var chosenCategory = chooseRandomCategory(categories);
+      var chosenCategoryShortName = "'" + chosenCategory.short_name + "'";  // Оборачиваем в кавычки
 
-  // Загрузка элементов меню
-  dc.loadMenuItems = function (categoryShort) {
-    showLoading("#main-content");
-    $ajaxUtils.sendGetRequest(
-      menuItemsUrl + categoryShort + ".json",
-      buildAndShowMenuItemsHTML
-    );
-  };
+      // STEP 3: Подставляем значение случайной категории в homeHtml
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
-  // Построение HTML для страницы категорий
-  function buildAndShowCategoriesHTML(categories) {
-    $ajaxUtils.sendGetRequest(
-      categoriesTitleHtml,
-      function (categoriesTitleHtml) {
-        $ajaxUtils.sendGetRequest(
-          categoryHtml,
-          function (categoryHtml) {
-            switchMenuToActive();
-            var categoriesViewHtml = buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml);
-            insertHtml("#main-content", categoriesViewHtml);
-          },
-          false
-        );
-      },
-      false
-    );
-  }
+      // STEP 4: Вставляем измененный HTML в основной контент
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+    },
+    false
+  );
+}
 
-  // Построение HTML для представления категорий
-  function buildCategoriesViewHtml(categories, categoriesTitleHtml, categoryHtml) {
-    var finalHtml = categoriesTitleHtml;
-    finalHtml += "<section class='row'>";
-    for (var i = 0; i < categories.length; i++) {
-      var html = categoryHtml;
-      var name = categories[i].name;
-      var short_name = categories[i].short_name;
-      html = insertProperty(html, "name", name);
-      html = insertProperty(html, "short_name", short_name);
-      finalHtml += html;
-    }
-    finalHtml += "</section>";
-    return finalHtml;
-  }
+// Given array of category objects, returns a random category object.
+function chooseRandomCategory(categories) {
+  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+  return categories[randomArrayIndex];
+}
 
-  // Построение HTML для страницы элементов меню
-  function buildAndShowMenuItemsHTML(categoryMenuItems) {
-    $ajaxUtils.sendGetRequest(
-      menuItemsTitleHtml,
-      function (menuItemsTitleHtml) {
-        $ajaxUtils.sendGetRequest(
-          menuItemHtml,
-          function (menuItemHtml) {
-            switchMenuToActive();
-            var menuItemsViewHtml = buildMenuItemsViewHtml(categoryMenuItems, menuItemsTitleHtml, menuItemHtml);
-            insertHtml("#main-content", menuItemsViewHtml);
-          },
-          false
-        );
-      },
-      false
-    );
-  }
+// Load the menu categories view
+dc.loadMenuCategories = function () {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
+};
 
-  // Построение HTML для представления элементов меню
-  function buildMenuItemsViewHtml(categoryMenuItems, menuItemsTitleHtml, menuItemHtml) {
-    menuItemsTitleHtml = insertProperty(menuItemsTitleHtml, "name", categoryMenuItems.category.name);
-    menuItemsTitleHtml = insertProperty(menuItemsTitleHtml, "special_instructions", categoryMenuItems.category.special_instructions);
+// Load the menu items view
+dc.loadMenuItems = function (categoryShort) {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(menuItemsUrl + categoryShort + ".json", buildAndShowMenuItemsHTML);
+};
 
-    var finalHtml = menuItemsTitleHtml;
-    finalHtml += "<section class='row'>";
-    var menuItems = categoryMenuItems.menu_items;
-    var catShortName = categoryMenuItems.category.short_name;
-
-    for (var i = 0; i < menuItems.length; i++) {
-      var html = menuItemHtml;
-      html = insertProperty(html, "short_name", menuItems[i].short_name);
-      html = insertProperty(html, "catShortName", catShortName);
-      html = insertItemPrice(html, "price_small", menuItems[i].price_small);
-      html = insertItemPortionName(html, "small_portion_name", menuItems[i].small_portion_name);
-      html = insertItemPrice(html, "price_large", menuItems[i].price_large);
-      html = insertItemPortionName(html, "large_portion_name", menuItems[i].large_portion_name);
-      html = insertProperty(html, "name", menuItems[i].name);
-      html = insertProperty(html, "description", menuItems[i].description);
-
-      // Добавить clearfix после каждого второго элемента меню
-      if (i % 2 !== 0) {
-        html += "<div class='clearfix visible-lg-block visible-md-block'></div>";
-      }
-      finalHtml += html;
-    }
-    finalHtml += "</section>";
-    return finalHtml;
-  }
-
-  // Добавление знака доллара к цене
-  function insertItemPrice(html, pricePropName, priceValue) {
-    if (!priceValue) {
-      return insertProperty(html, pricePropName, "");
-    }
-    priceValue = "$" + priceValue.toFixed(2);
-    html = insertProperty(html, pricePropName, priceValue);
-    return html;
-  }
-
-  // Добавление названия порции
-  function insertItemPortionName(html, portionPropName, portionValue) {
-    if (!portionValue) {
-      return insertProperty(html, portionPropName, "");
-    }
-    portionValue = "(" + portionValue + ")";
-    html = insertProperty(html, portionPropName, portionValue);
-    return html;
-  }
-
-  global.$dc = dc;
+// Остальной код остается без изменений...
+global.$dc = dc;
 
 })(window);
